a41dbf30aa11690d6f603d092e60904d891335d9
yuktnk <yuki0wl@gmail.com>
2020-09-17 10:51:32 +0900

    Update index.js
    BoardコンポーネントからSquareコンポーネントにpropsを渡す。
    （props = propertiesの略）

--- src/index.js
+++ src/index.js
@@ -2,19 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 
+// 正方形のマス目
 class Square extends React.Component {
   render() {
     return (
       <button className="square">
-        {/* TODO */}
+        {this.props.value}
       </button>
     );
   }
 }
 
+// 盤面
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i}/>;
   }
 
   render() {
@@ -43,6 +45,7 @@ class Board extends React.Component {
   }
 }
 
+// 三目並べ全体
 class Game extends React.Component {
   render() {
     return (
