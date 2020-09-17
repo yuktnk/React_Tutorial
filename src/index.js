494329caca96d86e4b0229aa794f04c6a3afdfc2
yuktnk <yuki0wl@gmail.com>
2020-09-17 12:30:11 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,14 +17,22 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null), // 要素数9の配列を作成、各要素をnullにする
-      
+      xIsNext: true, // どちらのプレイヤーの手番なのかを判定する際に使う真偽値
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice(); // slice();と引数を指定しないことで、配列全体を切り出す。
-    squares[i] = 'X';
-    this.setState({squares: squares})
+    
+    if (calculateWinner(squares) || squares[i]) {
+      return; // ゲームの決着が既についている場合やクリックされたマス目が既に埋まっている場合に早期に return する
+    }
+    
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    })
   }
 
   renderSquare(i) {
@@ -37,7 +45,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = ' Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -85,3 +99,23 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
